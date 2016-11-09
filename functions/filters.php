8a853ade9f374880ab6175285c215422414baa03
JClerc <jices25@gmail.com>
2016-11-09 16:42:03 +0100

    Add twig filter: has_category

--- functions/filters.php
+++ functions/filters.php
@@ -5,6 +5,17 @@ add_filter('timber_context', function ($context) {
     return $context;
 });
 
+add_filter('get_twig', function ($twig) {
+
+    $twig->addFunction(new Twig_SimpleFunction('has_category', 'has_category'));
+
+    $twig->addFunction(new Twig_SimpleFunction('debug', function ($obj) {
+        var_dump($obj);
+    }));
+
+    return $twig;
+});
+
 add_filter('acf/fields/google_map/api', function ($api) {
     $api['key'] = 'AIzaSyBImy0ia-fa4Dx8TDop4U6DnESFOErpzWY';
     return $api;
